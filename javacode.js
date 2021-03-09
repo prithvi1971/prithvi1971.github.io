4ba299c1915642566db48134f5ddd28abb1f5dcf
prithvi <jumaniaborad1971@gmail.com>
2021-03-09 01:15:05 -0500

    updated slides

--- javacode.js
+++ javacode.js
@@ -23,3 +23,58 @@ menu_item.forEach((item) => {
 		mobile_menu.classList.toggle('active');
 	});
 });
+
+// testing slideshow code below _______________________________________________________________________________________
+function showSlides(n) {
+	var slides_name= 'mySlides';
+  var i;
+  var slides = document.getElementsByClassName(slides_name);
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1;}    
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+// --------------------------------------------part 2 slides--------------------------------
+function showSlides2(n) {
+	var slides_name= 'bigSlides';
+  var i;
+  var slides = document.getElementsByClassName(slides_name);
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex = 1;}    
+  if (n < 1) {slideIndex = slides.length;}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+// testing slideshow code above ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function plusSlides2(n) {
+  showSlides2(slideIndex += n);
+}
+
+function currentSlide2(n) {
+  showSlides2(slideIndex = n);
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+showSlides2(slideIndex);
+
